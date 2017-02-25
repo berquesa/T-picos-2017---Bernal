56b2609c00b06b7e47f1dbd7f8a144e77ce0f0b5
laboratorio <laboratorio@LAB09-12>
2017-02-25 16:26:49 -0600

    Se agrega propiedad de Peso en kilos y Proyecto de pruebas funcionales para verificar la correctitud de ellas

--- Ulatina.Topicos.Calentamiento/Ulatina.Topicos.AdventureWorks.Model/Tables/Product.cs
+++ Ulatina.Topicos.Calentamiento/Ulatina.Topicos.AdventureWorks.Model/Tables/Product.cs
@@ -8,7 +8,9 @@ namespace Ulatina.Topicos.AdventureWorks.Model
 {
     public partial class Product
     {
-        public string SizeInInches{ get
+        public string SizeInInches
+        {
+            get
             {
                 double laConversion;
                 double lasPulgadas;
@@ -17,8 +19,8 @@ namespace Ulatina.Topicos.AdventureWorks.Model
                 {
                     if (double.TryParse(Size, out laConversion))
                     {
-                        lasPulgadas = laConversion / 2.54;
-                        elResultado = string.Format("{0} in.", lasPulgadas.ToString());
+                        lasPulgadas = Math.Round(laConversion / 2.54, 2);
+                        elResultado = string.Format("{0} in", lasPulgadas.ToString());
                     }
                     else
                         elResultado = "error";
@@ -26,6 +28,34 @@ namespace Ulatina.Topicos.AdventureWorks.Model
                 else
                     elResultado = "unidad desconocida";
                 return elResultado;
-            } }
+            }
+        }
+
+        public string WeigthInKilos
+        {
+            get
+            {
+                decimal? losKilos;
+                string elResultado = string.Empty;
+                switch (WeightUnitMeasureCode)
+                {
+                    case "":
+                        elResultado = Weight.ToString();
+                        break;
+                    case "G":
+                        losKilos = (decimal?) Math.Round((double)Weight / 1000, 2);
+                        elResultado = string.Format("{0} kg", losKilos.ToString());
+                        break;
+                    case "LB":
+                        losKilos = (decimal?) Math.Round((double)Weight / 2.20462, 2);
+                        elResultado = string.Format("{0} kg", losKilos.ToString());
+                        break;
+                    default:
+                        elResultado = "Unidad Desconocida";
+                        break;
+                }
+                return elResultado;
+            }
+        }
     }
 }
---
+++ Ulatina.Topicos.Calentamiento/Ulatina.Topicos.AdventureWorks.ModelTests/Products.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ulatina.Topicos.AdventureWorks.ModelTests
+{
+    [TestClass]
+    public class Products
+    {
+        [TestMethod]
+        public void ConvertirVeincintoCentimetrosEnPulgadas()
+        {
+            //preparacion del escenario
+            var elProducto = new Model.Product();
+            elProducto.SizeUnitMeasureCode = "CM";
+            elProducto.Size = "25";
+            var elResultadoEsperado = "9.84 in";
+
+            //ejecuto el metodo correspondiente
+            var elResultadoActual = elProducto.SizeInInches;
+
+            //verifico la correctitud del resultado
+            Assert.AreEqual(elResultadoEsperado, elResultadoActual);
+        }
+    }
+}
